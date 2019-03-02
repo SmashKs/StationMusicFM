208cb1c50cc5d961c81da9fdc41f7807809043c0
Jieyi <pokkbaby@gmail.com>
2019-03-02 19:21:31 +0900

    Feature: added the silent live data when first time observe.

---
+++ presentation/src/main/java/com/no1/taiwan/stationmusicfm/utils/aac/livedata/SilentLiveData.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2019 The Smash Ks Open Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.no1.taiwan.stationmusicfm.utils.aac.livedata
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+abstract class SilentLiveData<T> : LiveData<T>(), SilentHook<T> {
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner, observer)
+        try {
+            beSilent(observer)
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun beSilent(observer: Observer<in T>) {
+        // Get wrapper's version.
+        val classLiveData = LiveData::class.java
+        val fieldObservers = classLiveData.getDeclaredField("mObservers").apply { isAccessible = true }
+        val objectObservers = fieldObservers.get(this)
+        val classObservers = objectObservers.javaClass
+        val methodGet = classObservers.getDeclaredMethod("get", Any::class.java).apply { isAccessible = true }
+        val objectWrapperEntry = methodGet.invoke(objectObservers, observer)
+        lateinit var objectWrapper: Any
+        if (objectWrapperEntry is Map.Entry<*, *>) {
+            objectWrapper = requireNotNull(objectWrapperEntry.value)
+        }
+        val classObserverWrapper = objectWrapper.javaClass.superclass
+        val fieldLastVersion = requireNotNull(classObserverWrapper?.getDeclaredField("mLastVersion"))
+        fieldLastVersion.isAccessible = true
+        // Get LiveData's version.
+        val fieldVersion = classLiveData.getDeclaredField("mVersion").apply { isAccessible = true }
+        val objectVersion = fieldVersion.get(this)
+        // Set wrapper's version.
+        fieldLastVersion.set(objectWrapper, objectVersion)
+    }
+}
