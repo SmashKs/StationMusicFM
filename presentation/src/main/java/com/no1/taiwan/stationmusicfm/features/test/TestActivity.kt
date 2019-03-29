f60f17c34c4f514c0f7acff2ab7d764bc6135648
Jieyi <pokkbaby@gmail.com>
2019-03-29 14:08:10 +0900

    Feature: finished the animation for activity.

--- presentation/src/main/java/com/no1/taiwan/stationmusicfm/features/test/TestActivity.kt
+++ presentation/src/main/java/com/no1/taiwan/stationmusicfm/features/test/TestActivity.kt
@@ -22,11 +22,13 @@
 package com.no1.taiwan.stationmusicfm.features.test
 
 import android.os.Bundle
+import android.view.ViewTreeObserver
 import android.widget.ImageView
 import com.devrapid.kotlinknifer.extraNotNull
 import com.devrapid.kotlinknifer.logw
 import com.no1.taiwan.stationmusicfm.R
 import com.no1.taiwan.stationmusicfm.bases.AdvActivity
+import com.no1.taiwan.stationmusicfm.utils.imageview.loadByAny
 import org.jetbrains.anko.find
 
 class TestActivity : AdvActivity<TestViewModel>() {
@@ -36,6 +38,11 @@ class TestActivity : AdvActivity<TestViewModel>() {
     private val w by extraNotNull<Int>("w")
     private val h by extraNotNull<Int>("h")
     private val uri by extraNotNull<String>("uri")
+    private val pic by lazy { find<ImageView>(R.id.iv_pic) }
+    private var sX = 0
+    private var sY = 0
+    private var scaleX = 0f
+    private var scaleY = 0f
 
     /**
      * Initialize doing some methods and actions.
@@ -43,19 +50,48 @@ class TestActivity : AdvActivity<TestViewModel>() {
      * @param savedInstanceState previous state after this activity was destroyed.
      */
     override fun init(savedInstanceState: Bundle?) {
-        logw(x, y, w, h, uri)
         if (savedInstanceState == null) {
-            val observer = find<ImageView>(R.id.iv_pic).viewTreeObserver
-            observer.addOnPreDrawListener {
-                true
-            }
-        }
+            val observer = pic.viewTreeObserver
+            observer.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    pic.viewTreeObserver.removeOnPreDrawListener(this)
+                    val location = IntArray(2)
+                    pic.getLocationOnScreen(location)
 
-        find<ImageView>(R.id.iv_pic)
+                    sX = x - location[0]
+                    sY = y - location[1]
+
+                    scaleX = w.toFloat() / pic.width
+                    scaleY = h.toFloat() / pic.height
+
+                    runEnterAnimation()
+                    return true
+                }
+            })
+        }
     }
 
     override fun finish() {
         super.finish()
         overridePendingTransition(0, 0)
+    }
+
+    fun runEnterAnimation() {
+        pic.apply {
+            pivotX = 0f
+            pivotY = 0f
+            scaleX = this@TestActivity.scaleX
+            scaleY = this@TestActivity.scaleY
+            translationX = sX.toFloat()
+            translationY = sY.toFloat()
+            loadByAny(uri)
+        }
+
+        pic.animate()
+            .setDuration(500)
+            .scaleX(1f)
+            .scaleY(1f)
+            .translationX(0f)
+            .translationY(0f)
     }
 }
